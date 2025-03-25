f48b136864fb4f27a7a8ed86a60ed408a0db8db1
ahmed altaweel <abohmaid77@gmail.com>
2025-03-25 22:10:27 +0300

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createCanvas, loadImage } = require('canvas');
+const { createCanvas, loadImage ,registerFont } = require('canvas');
 const fs = require('fs');
 const path = require('path');
 
@@ -8,6 +8,9 @@ var port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+registerFont(path.join(__dirname, 'public', 'fonts', 'rb.ttf'), { family: 'rb' });
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -27,7 +30,7 @@ app.post('/add-text', async (req, res) => {
         ctx.drawImage(image, 0, 0);
 
         // إضافة النص إلى الصورة
-        ctx.font = '50px Arial';
+        ctx.font = '70px rb';
         ctx.fillStyle = 'white';
         ctx.textAlign = 'center';
         ctx.fillText(name, canvas.width / 2, 1300);
@@ -38,7 +41,9 @@ app.post('/add-text', async (req, res) => {
         stream.pipe(out);
 
         out.on('finish', () => {
-            res.send(`<a href="/output.jpg" download>Download Image</a>`);
+            // res.send(`<html><body><a href="/output.jpg" download>Download Image</a> </body></html>`);
+            res.download(outputPath, 'output.jpg'); 
+
         });
     } catch (error) {
         console.error(error);
